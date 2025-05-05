cd6d092b5d94cc852e02df012ff247fbcdaaa65e
HARSHv95 <harsh.dinesh.23031@iitgoa.ac.in>
2025-05-05 23:44:39 +0530

    Final Commit

--- src/components/socketConnection.jsx
+++ src/components/socketConnection.jsx
@@ -8,7 +8,7 @@ export const SocketProvider = ({children})=>{
 
     const connectSocket = ()=>{
         if(!socket){
-            const newSocket = io("https://project-server-h1po.onrender.com");
+            const newSocket = io("http://localhost:3000");
             setsocket(newSocket);
             return newSocket;
         }
